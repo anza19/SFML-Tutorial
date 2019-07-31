e8820d4c38d8d7de6df41decb00754dc7fac68fb
anza19 <anza.k.9539@gmail.com>
2019-07-31 14:25:02 -0400

    Added a mini snake game.

--- keyboard-input/main.cpp
+++ keyboard-input/main.cpp
@@ -9,14 +9,17 @@ int main(){
     //Here we are rendering the window in video mode
     //For videomode we need to give two parameters, the dimensions of the screen and the name
     //We can also give the style of the window
-    sf::RenderWindow window(sf::VideoMode(512, 512),"SFML Tutorials", sf::Style::Close | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(1024, 1024),"SFML Tutorials", sf::Style::Default);
+    window.clear();
 
     //Making a simple rectangle
     //For size we give a vector ==> x and y coordinate, these can suffice for width and height
-    sf::RectangleShape player(sf::Vector2f(50.0f, 50.0f));
+    // sf::RectangleShape player(sf::Vector2f(50.0f, 50.0f));
 
+    //Making a simple circle
+    sf::CircleShape player(10.0f, 30UL);
     //We can fill the color for the player
-    player.setFillColor(sf::Color::Magenta);
+    player.setFillColor(sf::Color::Green);
 
     //Need a game loop
     while(window.isOpen()){
@@ -30,7 +33,7 @@ int main(){
 
                 //First we check for the closed event
                 case sf::Event::Closed:
-                    window.clear();
+                    // window.clear();
                     window.close();
                     break;
 
@@ -49,9 +52,32 @@ int main(){
                     }
                     break;
             }
+        }   
+
+        //Here we take the input from a user:
+        //Here we will move the snake to the right
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+            player.move(0.01f, 0.0f);
         }
+
+        //Move the snake down
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+            player.move(0.0f, 0.01f);
+        }
+
+        //Move the snake left
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+            player.move(-0.01f, 0.0f);
+        }
+
+        //Move the snake up
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+            player.move(0.0f, -0.01f);
+        }
+
+
         //clear the screen after every frame
-        window.clear();
+        // window.clear();
         
         //draw player
         window.draw(player);
